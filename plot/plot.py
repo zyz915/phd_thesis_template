170ae1f7d09b9a574c8e3b559ff4bbab177dbd74
Yongzhe Zhang <zyz915@gmail.com>
2017-05-30 05:17:39 +0900

    add new experiments

--- plot/plot.py
+++ plot/plot.py
@@ -54,12 +54,48 @@ def parse(fname):
     f.close()
     return dic
 
+def patch(fname, dic):
+    f = file(fname, "r")
+    #print fname
+    while (True):
+        line = f.readline();
+        if (not line): break;
+        if (line.startswith("$")):
+            cont = line.strip().split(" ")
+            prog = cont[1]
+            data = cont[2]
+            core = int(cont[3])
+            time = int(cont[4])
+            step = 0
+            while (True):
+                line = f.readline()
+                if (line.startswith("Dump")):
+                    break
+                if (line.startswith("Superstep")):
+                    step += 1
+                if (line.startswith("Communication Time :")):
+                    comm = float(line.split(" ")[3])
+                if (line.startswith("Communication Time:")):
+                    comm = float(line.split(" ")[2])
+                if (line.startswith("Total Computational")):
+                    comp = float(line.split(" ")[4])
+                if (line.startswith("Total Running")):
+                    comp = float(line.split(" ")[3])
+            #print prog, data, time, core, comp, comm, step
+            #print prog, data, dic[prog][data][core][time], (core, comp, comm, step)
+            if (comp < dic[prog][data][core][time][1]):
+                dic[prog][data][core][time] = (core, comp, comm, step)
+    f.close()
+    return dic
+
 dic0 = parse("raw-pregel.txt");
 dic1 = parse("raw-palgol.txt");
 dic2 = parse("raw-custom.txt");
 
+patch("raw-custom-dg.txt", dic2);
+patch("raw-custom-sssp.txt", dic2);
+
 cores = [4, 8, 12, 16]
-M = 3.0
 
 # print value
 for prog in dic0:
@@ -69,14 +105,23 @@ for prog in dic0:
     for data in d0:
         lst = []
         out = prog + " : " + data
+        mx = 1.0
+        mi = 1.0
         for c in cores:
+            if (c == 4): continue;
             out += (" & %.2f" % (get(d0[data][c], 1)))
             out += (" & %.2f" % (get(d1[data][c], 1)))
             out += (" & %.2f" % (get(d2[data][c], 1)))
+            if (get(d2[data][c], 1) > get(d0[data][c], 1)):
+                mx = max(mx, get(d2[data][c], 1) / get(d0[data][c], 1))
+            else:
+                mi = min(mi, get(d2[data][c], 1) / get(d0[data][c], 1))
         out += " \\\\"
         print out
+        print "%.2f%% %.2f%%" % (mi * 100.0 - 100, mx * 100 - 100)
 print ""
 
+exit()
 
 # bar plot for execution and communication
 '''
@@ -118,8 +163,7 @@ for prog0 in dic0:
         plt.clf()
 '''
 
-exit()
-
+'''
 for prog in dic0:
     lst = {}
     for data in dic1[prog]:
@@ -131,33 +175,27 @@ for prog in dic0:
         x = lst[data][0]
         y = lst[data][1]
         print prog, data, lst[data], ("%.1f%%" % ((x-y)*100./y))
+'''
 
-for prog0 in dic0:
-    tot = 1 + len(mp[prog0])
-    for data in dic0[prog0]:
-        base = -tot * 0.5
-        d = dic0[prog0]
-        x  = []
-        y1 = []
-        for c in [0, 1, 2, 3]:
-            x.append(d[data][c][0]/M+base*0.5)
-            y1.append(d[data][c][3])
-        plt.title(prog0 + " " + data)
-        plt.bar(x, y1, 0.4, color=colors[0], label="pregel")
-        plt.xticks([x/M-0.05 for x in cores], cores)
-        plt.xlim(xmin=4/M-0.15-tot*0.25, xmax=16/M+0.05+tot*0.25)
-        shf = 1
-        for prog1 in mp[prog0]:
-            d = dic1[prog1]
-            x  = []
-            y1 = []
-            for c in [0, 1, 2, 3]:
-                x.append(d[data][c][0]/M+(base+shf)*0.5)
-                y1.append(d[data][c][3])
-            plt.bar(x, y1, 0.4, color=colors[shf], label="palgol")
-            shf += 1
-        plt.xlabel("number of cores")
-        plt.ylabel("number of supersteps")
-        plt.legend(loc="lower right")
-        plt.savefig("step_" + prog0 + "_" + data + ".pdf")
+dics = [dic0, dic1, dic2]
+names = ["Pregel", "PalgolA", "PalgolB"]
+app = {"sssp":"SSSP", "pagerank":"PageRank", "svppa":"S-V", "msf":"MSF"}
+for prog in dic0:
+    for data in dic0[prog]:
+        graph = data
+        if (data == "3d20m"): graph = "Random"
+        plt.title("Running " + app[prog] + " on " + graph, fontsize="24")
+        for idx in range(0, 3):
+            d = dics[idx][prog]
+            x = []
+            y = []
+            for c in cores:
+                x.append(c / 2 + idx * 0.5 - 0.7)
+                y.append(get(d[data][c], 1))
+            plt.bar(x, y, 0.4, color=colors[idx], label=names[idx])
+        plt.xticks([2, 4, 6, 8], cores)
+        plt.xlabel("Number of Cores", fontsize="20")
+        plt.ylabel("Execution Time (s)", fontsize="22")
+        plt.legend(loc="upper right")
+        plt.savefig("exec_" + prog + "_" + data + ".pdf")
         plt.clf()
